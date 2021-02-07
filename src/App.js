2b7a807f3a205f67bd03bba5b75e03214d2406c5
kristiantg <kristiantgregersen@gmail.com>
2021-02-08 00:48:02 +0100

    homepage done

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import Sidebar from './components/sidebar/sidebar.js';
 import Console from './components/console/console.js';
 import Particles from './components/particles/particles.js'
 import Hometext from './components/hometext/hometext.js'
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Hometext/>
-      <Sidebar/>
-      <Console/>
-      <Particles/>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/about">
+        <Sidebar/>
+        <Console/>
+
+        </Route>
+        <Route path="/skills">
+        <Sidebar/>
+        <Console/>
+          
+        </Route>
+        <Route path="/contact">
+        <Sidebar/>
+        <Console/>
+          
+        </Route>
+        <Route path="/">
+            <Hometext/>
+            <Sidebar/>
+            <Console/>
+            <Particles/>
+        </Route>
+      </Switch>
+  </Router>
   );
 }
 
--- src/components/hometext/hometext.js
+++ src/components/hometext/hometext.js
@@ -1,31 +1,88 @@
 import React, { Component } from 'react';
 import './hometext.css'
 
-class Hometext extends Component {
-    componentDidMount(){
-        const element = document.querySelector('.hometext');
+async function fadeIn(elements){
+    let duration = 0.4;
+    Array.from(elements).forEach(function(element) {
+        debugger;
+        duration += 0.1;
+        let durationdelay = duration.toString() + 's';
+        element.style.setProperty('--animate-duration', durationdelay);
+        element.classList.add('animate__animated', 'animate__fadeInLeft');
+
 
         element.addEventListener('animationend', () => {
-            element.classList.remove('animate__animated', 'animate__bounce');
+            element.classList.remove('animate__animated', 'animate__fadeInLeft');
+          });
+  });
+}
+
+class Hometext extends Component {
+    componentDidMount(){
+        const elements = document.querySelectorAll('.bouncer');
+        
+        fadeIn(elements);
+
+        Array.from(elements).forEach(function(element) {
+            element.addEventListener('mouseover', function() {
+                element.style.setProperty('--animate-duration', '1s');
+                element.classList.add('animate__animated', 'animate__headShake');
+              });
           });
 
-        element.addEventListener('mouseover', function() {
-            console.log('Event triggered');
-            element.classList.add('animate__animated', 'animate__bounce');
+          Array.from(elements).forEach(function(element) {
+            element.addEventListener('animationend', () => {
+                element.classList.remove('animate__animated', 'animate__headShake');
+              });
           });
     }
     render() {
         return (
         <main>
-            <span className="hometext">h</span>
-            <span className="hometext">e</span>
-            <h1 className="helloworld">
-                hello world<br/>
-                i am kristian, software developer<br/>
-            </h1>
-            <span className="helloworld"> 
-                test
-            </span>
+            <div class="flex-container">
+                <div className="bouncer">h</div>
+                <div className="bouncer">e</div>
+                <div className="bouncer">l</div>
+                <div className="bouncer">l</div>
+                <div className="bouncer">o</div>
+                <div className="bouncer"> </div>
+                <div className="bouncer">w</div>
+                <div className="bouncer">o</div>
+                <div className="bouncer">r</div>
+                <div className="bouncer">l</div>
+                <div className="bouncer">d</div>
+                <div className="break"></div>
+                <div className="bouncer">i</div>
+                <div className="bouncer">m</div>
+                <div className="bouncer"> </div>
+                <div className="bouncer">k</div>
+                <div className="bouncer">r</div>
+                <div className="bouncer">i</div>
+                <div className="bouncer">s</div>
+                <div className="bouncer">t</div>
+                <div className="bouncer">i</div>
+                <div className="bouncer">a</div>
+                <div className="bouncer">n</div>
+                <div className="break"></div>
+                <div className="bouncer">s</div>
+                <div className="bouncer">o</div>
+                <div className="bouncer">f</div>
+                <div className="bouncer">t</div>
+                <div className="bouncer">w</div>
+                <div className="bouncer">a</div>
+                <div className="bouncer">r</div>
+                <div className="bouncer">e</div>
+                <div className="bouncer"> </div>
+                <div className="bouncer">d</div>
+                <div className="bouncer">e</div>
+                <div className="bouncer">v</div>
+                <div className="bouncer">e</div>
+                <div className="bouncer">l</div>
+                <div className="bouncer">o</div>
+                <div className="bouncer">p</div>
+                <div className="bouncer">e</div>
+                <div className="bouncer">r</div>
+            </div>
         </main>
         );
     }
