ba5b0083a8282703cc127634a94b24e31f1bddd7
kristiantg <kristiantgregersen@gmail.com>
2021-02-09 23:18:08 +0100

    pog

--- src/components/cv/cv.js
+++ src/components/cv/cv.js
@@ -2,16 +2,58 @@ import React, { Component } from 'react';
 import './cv.css'
 
 class Cv extends Component {
+    componentDidMount(){
+        const elements = document.querySelectorAll('.bouncer');
+        
+        fadeIn(elements);
+
+        Array.from(elements).forEach(function(element) {
+            element.addEventListener('mouseover', function() {
+                element.style.setProperty('--animate-duration', '1s');
+                element.classList.add('animate__animated', 'animate__headShake');
+              });
+          });
+
+          Array.from(elements).forEach(function(element) {
+            element.addEventListener('animationend', () => {
+                element.classList.remove('animate__animated', 'animate__headShake');
+              });
+          });
+    }
+
     render() {
         return (
-            <div className="animate__animated animate__fadeIn cv">
-                <div className="about_me_titlte">
-                    about me
-                </div>
-                <hr></hr>
+            <main>
+            <div class="flex-container">
+                <div className="bouncer">a</div>
+                <div className="bouncer">b</div>
+                <div className="bouncer">o</div>
+                <div className="bouncer">u</div>
+                <div className="bouncer">t</div>
+                <div className="bouncer"> </div>
+                <div className="bouncer">m</div>
+                <div className="bouncer">e</div>
+                <div className="breaker"></div>
             </div>
+            <div><hr className="about_me_title animate__animated animate__fadeIn"></hr></div>
+        </main>
         );
     }
+}
+
+async function fadeIn(elements){
+    let duration = 0.4;
+    Array.from(elements).forEach(function(element) {
+        duration += 0.1;
+        let durationdelay = duration.toString() + 's';
+        element.style.setProperty('--animate-duration', durationdelay);
+        element.classList.add('animate__animated', 'animate__fadeInLeft');
+
+
+        element.addEventListener('animationend', () => {
+            element.classList.remove('animate__animated', 'animate__fadeInLeft');
+          });
+  });
 }
 
 export default Cv;
